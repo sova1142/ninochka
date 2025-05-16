aade74e8d2c20402ce934ed6584042cda571108d
sova1142 <sova1142@yandex.ru>
2025-05-16 13:44:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv(8084573722:AAGSWWej0tSfH6-eBvQDG-W444tM2AQ0KqA)
-QWEN_API_KEY = os.getenv(eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEwZDg3YjhlLWFkNTgtNDBmZS05YWQ2LTQ5MTk2MDI1MDFmZiIsImV4cCI6MTc0OTc5NjMyNn0.CZgXzy2cQLfDczanuQ9p6TZBSRu1VdEF7V2XLpn2t84)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+QWEN_API_KEY = os.getenv("QWEN_API_KEY")
 
 QUESTS_DIR = "quests"
 PROFILES_DIR = "profiles"
