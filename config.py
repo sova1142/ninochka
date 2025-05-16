3c2ae6741bf3d4e448f3d2d879571ba47de7d4ae
sova1142 <sova1142@yandex.ru>
2025-05-16 14:20:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-QWEN_API_KEY = os.getenv("QWEN_API_KEY")
-
-QUESTS_DIR = "quests"
-PROFILES_DIR = "profiles"
-
-RELATIONSHIP_LEVELS = {
-    0: "незнакомец",
-    1: "друг",
-    2: "близкий друг",
-    3: "романтические отношения",
-    4: "возлюбленная"
-}
